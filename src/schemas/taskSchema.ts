8329135b57a613a6b4c4f3980878ac33b83699d5
rapon1kt <shinnatensei@gmail.com>
2026-05-09 16:24:29 -0300

    feat: creating taskSchema with zod

---
+++ src/schemas/taskSchema.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+
+const taskSchema = z.object({
+  title: z
+    .string()
+    .min(3, { error: "The title must be at least 3 characters long." })
+    .max(50, { error: "The title cannot exceed 50 characters." }),
+  description: z
+    .string()
+    .max(300, { error: "The description cannot exceed 300 characters." })
+    .optional()
+    .or(z.literal("")),
+  severity: z
+    .enum(["low", "medium", "high"], {
+      error: "Select a valid severity.",
+    })
+    .optional()
+    .or(z.literal("")),
+  dueDate: z
+    .string()
+    .optional()
+    .transform((val) => (val === "" ? undefined : val))
+    .transform((val) => (val ? new Date(val) : undefined))
+    .refine(
+      (date) => {
+        if (!date) return true;
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        return date >= today;
+      },
+      { error: "The due date cannot be earlier than the current day." },
+    ),
+});
+
+export default taskSchema;
