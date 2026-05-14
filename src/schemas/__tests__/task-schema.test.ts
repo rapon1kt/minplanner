340d9b5b15ba91a0b6dcc1691d705514f717ade0
rapon1kt <shinnatensei@gmail.com>
2026-05-14 00:05:13 -0300

    test: ensure task-schema accepts optional fields as empty values

--- src/schemas/__tests__/task-schema.test.ts
+++ src/schemas/__tests__/task-schema.test.ts
@@ -20,4 +20,21 @@ describe("TaskSchema", () => {
       dueDate: new Date(2030, 2, 26),
     });
   });
+
+  it("Should accept optional fields as empty values.", () => {
+    const result = taskSchema.safeParse({
+      title: "valid_title",
+      description: "",
+      severity: undefined,
+      dueDate: "",
+    });
+
+    expect(result.success).toBe(true);
+    expect(result.data).toEqual({
+      title: "valid_title",
+      description: "",
+      severity: undefined,
+      dueDate: undefined,
+    });
+  });
 });
