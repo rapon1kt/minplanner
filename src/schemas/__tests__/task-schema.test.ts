1a3cd61991ef414682ca173bf3e09c45d5019b69
rapon1kt <shinnatensei@gmail.com>
2026-05-14 00:01:21 -0300

    test: ensure task-schema returns a valid task data

---
+++ src/schemas/__tests__/task-schema.test.ts
@@ -0,0 +1,23 @@
+import { taskSchema } from "@/schemas";
+import { describe, expect, it } from "vitest";
+
+const makeValidTaskData = () => ({
+  title: "valid_title",
+  description: "valid_description",
+  severity: "low",
+  dueDate: new Date(2030, 2, 26).toDateString(),
+});
+
+describe("TaskSchema", () => {
+  it("Should parse valid task data.", () => {
+    const result = taskSchema.safeParse(makeValidTaskData());
+
+    expect(result.success).toBe(true);
+    expect(result.data).toEqual({
+      title: "valid_title",
+      description: "valid_description",
+      severity: "low",
+      dueDate: new Date(2030, 2, 26),
+    });
+  });
+});
