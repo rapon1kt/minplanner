8b8cff40f9f6440c13a983e582d5bcdeaac1ccdb
rapon1kt <shinnatensei@gmail.com>
2026-05-14 13:54:49 -0300

    test: ensure deleteTaskSchema reject if taskId length have more than 24 chars

--- src/schemas/__tests__/delete-task-schema.test.ts
+++ src/schemas/__tests__/delete-task-schema.test.ts
@@ -11,10 +11,16 @@ describe("DeleteTaskSchema", () => {
     expect(result.success).toBe(true);
   });
 
-  it("Should reject if the id is less than 24 chars", () => {
+  it("Should reject if the id length have less than 24 chars", () => {
     const shortId = "507f1f77bcf86cd79943901";
     const result = deleteTaskSchema.safeParse({ taskId: shortId });
     expect(result.success).toBe(false);
+    expect(result.error?.issues[0].message).toBe("Invalid ObjectId.");
+  });
+
+  it("Should reject if the id length have more than 24 chars", () => {
+    const bigId = "507f1f77bcf86cd7994390112";
+    const result = deleteTaskSchema.safeParse({ taskId: bigId });
     expect(result.error?.issues[0].message).toBe("Invalid ObjectId.");
   });
 });
