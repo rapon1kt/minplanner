7c7dbc6f37135e5aceacad8ba4801ca1340ccc24
rapon1kt <shinnatensei@gmail.com>
2026-05-16 16:56:18 -0300

    test: ensure creatingTaskSchemas rules when updating

--- src/schemas/__tests__/update-task-schema.test.ts
+++ src/schemas/__tests__/update-task-schema.test.ts
@@ -53,4 +53,91 @@ describe("UpdateTaskSchema", () => {
       ]),
     );
   });
+
+  it("Should reject title with less than 3 characters.", ({ expect }) => {
+    const result = updateTaskSchema.safeParse({
+      ...makeValidUpdateTaskData(),
+      title: "12",
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["title"],
+          message: "The title must be at least 3 characters long.",
+        }),
+      ]),
+    );
+  });
+
+  it("Should reject title with more than 50 characters.", ({ expect }) => {
+    const result = updateTaskSchema.safeParse({
+      ...makeValidUpdateTaskData(),
+      title: ".".repeat(51),
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["title"],
+          message: "The title cannot exceed 50 characters.",
+        }),
+      ]),
+    );
+  });
+
+  it("Should reject description with more than 300 characters.", ({
+    expect,
+  }) => {
+    const result = updateTaskSchema.safeParse({
+      ...makeValidUpdateTaskData(),
+      description: ".".repeat(301),
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["description"],
+          message: "The description cannot exceed 300 characters.",
+        }),
+      ]),
+    );
+  });
+
+  it("Should reject invalid severity.", ({ expect }) => {
+    const result = updateTaskSchema.safeParse({
+      ...makeValidUpdateTaskData(),
+      severity: "invalid_severity",
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["severity"],
+          message: "Select a valid severity.",
+        }),
+      ]),
+    );
+  });
+
+  it("Should reject dueDate before the current day.", ({ expect }) => {
+    const result = updateTaskSchema.safeParse({
+      ...makeValidUpdateTaskData(),
+      dueDate: new Date(2007, 3, 26).toDateString(),
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["dueDate"],
+          message: "The due date cannot be earlier than the current day.",
+        }),
+      ]),
+    );
+  });
 });
