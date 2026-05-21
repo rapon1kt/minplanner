4d6a36a98298b5628c2648bc56ea23b6ae6f17bc
rapon1kt <shinnatensei@gmail.com>
2026-05-20 23:50:20 -0300

    feat: creating update-task-service

--- src/services/task/index.ts
+++ src/services/task/index.ts
@@ -1,2 +1,3 @@
 export { default as createTaskService } from "./create-task-service";
 export { default as deleteTaskService } from "./delete-task-service";
+export { default as updateTaskService } from "./update-task-service";
---
+++ src/services/task/update-task-service.ts
@@ -0,0 +1,39 @@
+import { NotFound } from "@/errors";
+import { AppError } from "@/errors/app-error";
+import { TaskModel } from "@/infra/db/mongoose/models";
+import { connectMongoose } from "@/infra/db/mongoose/mongoose";
+
+interface UpdateTaskDTO {
+  title?: string;
+  dueDate?: Date;
+  description?: string;
+  severity?: string;
+}
+
+export default async function updateTaskService(
+  taskId: string,
+  userId: string,
+  updateTaskDTO: UpdateTaskDTO,
+) {
+  await connectMongoose();
+
+  try {
+    const updatedTask = await TaskModel.findOneAndUpdate(
+      { _id: taskId, userId },
+      { $set: updateTaskDTO },
+      { new: true },
+    ).lean();
+    if (!updatedTask) {
+      throw new NotFound("Task not found.");
+    }
+    return { success: true, updatedTask };
+  } catch (error: unknown) {
+    if (error instanceof AppError) throw error;
+    console.error("Internal server error while updating task: ", error);
+    throw new AppError(
+      "It was not possible to update the task at this time.",
+      500,
+      "DATABASE_ERROR",
+    );
+  }
+}
