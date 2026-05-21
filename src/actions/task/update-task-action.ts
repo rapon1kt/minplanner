bb4138ffe6b450780e3e9c9242e7425f0d7d340b
rapon1kt <shinnatensei@gmail.com>
2026-05-21 11:09:55 -0300

    feat: creating update-task-action

---
+++ src/actions/task/update-task-action.ts
@@ -0,0 +1,64 @@
+import { AppError } from "@/errors/app-error";
+import { getVerifiedUser } from "@/lib/verify-auth";
+import { updateTaskSchema } from "@/schemas/taskSchema";
+import { updateTaskService } from "@/services/task";
+import { revalidatePath } from "next/cache";
+import z from "zod";
+
+type Properties = {
+  taskId?: { errors: string[] };
+};
+
+type UpdateTaskResponse = {
+  success: boolean;
+  message: string;
+  errors?: Properties;
+  errorCode?: string;
+};
+
+export default async function updateTaskAction(
+  prevState: unknown,
+  formData: FormData,
+): Promise<UpdateTaskResponse> {
+  const rawData = {
+    taskId: formData.get("taskId"),
+    title: formData.get("title"),
+    severity: formData.get("severity"),
+    description: formData.get("description"),
+    dueDate: formData.get("dueDate"),
+  };
+
+  const validatedFields = updateTaskSchema.safeParse(rawData);
+
+  if (!validatedFields.success) {
+    return {
+      success: false,
+      errors: z.treeifyError(validatedFields.error).properties,
+      message: "Invalid fields.",
+    };
+  }
+
+  try {
+    const { id: userId } = await getVerifiedUser();
+
+    const { taskId, ...updateData } = validatedFields.data;
+    await updateTaskService(taskId, userId, updateData);
+    revalidatePath("/");
+    return { success: true, message: "Task updated with success." };
+  } catch (error: unknown) {
+    if (error instanceof AppError) {
+      return {
+        success: false,
+        message: error.message,
+        errorCode: error.code,
+      };
+    }
+
+    console.error("An unexpected error occurred: ", error);
+
+    return {
+      success: false,
+      message: "Something went wrong while updating the task.",
+    };
+  }
+}
