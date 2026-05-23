6327e1b37400c78f5aee81a98e41036bbaccf308
rapon1kt <shinnatensei@gmail.com>
2026-05-23 01:40:49 -0300

    feat: exporting update-task-action from actions folder

--- src/actions/task/index.ts
+++ src/actions/task/index.ts
@@ -1,2 +1,3 @@
 export { default as createTaskAction } from "./create-task-action";
 export { default as deleteTaskAction } from "./delete-task-action";
+export { default as updateTaskAction } from "./update-task-action";
