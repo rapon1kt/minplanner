09263b84957a01f2889fe0c9bc9ad154b7fb1a54
rapon1kt <shinnatensei@gmail.com>
2026-05-23 01:43:14 -0300

    feat: adding tailwindcss configs to global and adding fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from "next";
+import { Barlow, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+
+const barlow = Barlow({
+  weight: ["100", "200", "400", "500"],
+  subsets: ["latin"],
+  variable: "--font-barlow",
+});
+
+const space = Space_Grotesk({
+  weight: ["600", "700"],
+  subsets: ["latin"],
+  variable: "--font-space",
+});
 
 export const metadata: Metadata = {
   title: "MinPlanner",
@@ -12,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="antialiased">
+    <html
+      lang="en"
+      className={`antialiased font-sans ${barlow.variable} ${space.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
