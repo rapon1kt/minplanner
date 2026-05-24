f01c7f44d89fbfe1399c777dea45948d9b5a604d
rapon1kt <shinnatensei@gmail.com>
2026-05-24 18:23:59 -0300

    feat: creating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <div>Home</div>;
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+import Header from "@/components/header";
+import HomeShell from "@/components/home-shell";
+import Footer from "@/components/footer";
+import { TaskModel } from "@/infra/db/mongoose/models";
+import { connectMongoose } from "@/infra/db/mongoose/mongoose";
+import sortTasks from "@/utils/sorters/sort-tasks";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/sign-in");
+  }
+
+  async function getTasks() {
+    "use server";
+    await connectMongoose();
+    const tasks = await TaskModel.find({
+      userId: session!.user.id,
+    }).lean();
+    return JSON.parse(JSON.stringify(sortTasks(tasks)));
+  }
+
+  const tasks = await getTasks();
+
+  return (
+    <div className="transition-all min-h-screen bg-neutral-950 text-neutral-100">
+      <div className="max-w-6xl mx-auto min-h-screen border-l border-r border-neutral-800">
+        <Header session={session} />
+        <HomeShell tasks={tasks} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
