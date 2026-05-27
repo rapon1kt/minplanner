cf703294f8706eb973106113620e03fbd2c63bee
rapon1kt <shinnatensei@gmail.com>
2026-05-27 13:31:10 -0300

    refactor: mapping taskCard for expired tasks and fixing some styles

--- src/components/task-container/task-card.tsx
+++ src/components/task-container/task-card.tsx
@@ -1,7 +1,13 @@
 import { deleteTask, updateTask } from "@/actions/task/form-actions";
 import { Task } from "@/core/domain/models";
 import { dateFormatter } from "@/utils";
-import { CheckCircle2, Circle, Skull, Trash2 } from "lucide-react";
+import {
+  AlarmClock,
+  CheckCircle2,
+  RefreshCcw,
+  Skull,
+  Trash2,
+} from "lucide-react";
 
 const severityTextColor = {
   low: "text-green-800",
@@ -18,53 +24,93 @@ const returnSeverityStyle = (severity: SeverityType) => {
 const capitalizeStr = (str: string): string =>
   str.charAt(0).toUpperCase() + str.slice(1);
 
+const renewTaskToToday = (dueDate: string): string | undefined => {
+  if (!dueDate) {
+    return undefined;
+  }
+  const today = new Date();
+  today.setHours(23, 59, 59, 999);
+  return today.toString();
+};
+
 export default function TaskCard({ task }: { task: Task }) {
   return (
     <div className="group flex items-center gap-4 p-4 bg-neutral-900/20 rounded-sm border border-neutral-800 hover:border-neutral-700 transition-all hover:bg-neutral-900/50">
-      <form action={updateTask}>
-        <input type="hidden" name="taskId" value={task._id?.toString()} />
-        <input
-          type="hidden"
-          name="isCompleted"
-          value={String(!task.isCompleted)}
-        />
-        <button
-          type="submit"
-          aria-label={
-            task.isCompleted ? "Mark task as pending" : "Complete task"
-          }
-          className="cursor-pointer shrink-0 transition-colors"
-        >
-          {task.isCompleted ? (
-            <CheckCircle2 size={24} className="text-green-900" />
-          ) : (
-            <Circle
-              size={24}
-              className="text-neutral-700 group-hover:text-neutral-600"
+      <div className="flex flex-col h-full items-center gap-2">
+        {task.isExpired && (
+          <form className="flex" action={updateTask}>
+            <input type="hidden" name="taskId" value={task._id?.toString()} />
+            <input type="hidden" name="isExpired" value="false" />
+            <input
+              type="hidden"
+              name="dueDate"
+              value={renewTaskToToday(task.dueDate!)}
             />
-          )}
-        </button>
-      </form>
-      <div className="flex-1">
-        <div className="flex space-x-2 items-center">
-          <p
-            className={`flex text-lg items-center gap-2 font-barlow transition-all ${task.isCompleted ? "line-through text-neutral-500" : "text-neutral-400"}`}
-          >
-            {task.title}
-          </p>
-          <span className="text-neutral-400">•</span>
-          {task.severity && (
-            <p
-              className={`font-barlow ${returnSeverityStyle(task.severity as SeverityType)}`}
+            <button
+              type="submit"
+              className="cursor-pointer shrink-0 transition-colors"
             >
-              {capitalizeStr(task.severity)}
+              <RefreshCcw
+                size={24}
+                className="text-neutral-700 group-hover:text-neutral-600 hover:text-indigo-900"
+              />
+            </button>
+          </form>
+        )}
+        <form className="flex" action={updateTask}>
+          <input type="hidden" name="taskId" value={task._id?.toString()} />
+          <input
+            type="hidden"
+            name="isCompleted"
+            value={String(!task.isCompleted)}
+          />
+          <button
+            type="submit"
+            aria-label={
+              task.isCompleted ? "Mark task as pending" : "Complete task"
+            }
+            className="cursor-pointer shrink-0 transition-colors"
+          >
+            {task.isCompleted ? (
+              <CheckCircle2 size={24} className="text-green-900" />
+            ) : (
+              <CheckCircle2
+                size={24}
+                className="text-neutral-700 hover:text-green-900 group-hover:text-neutral-600"
+              />
+            )}
+          </button>
+        </form>
+      </div>
+      <div className="flex-1 space-y-2">
+        <div>
+          <div className="flex space-x-2 items-center">
+            <p
+              className={`flex text-lg items-center gap-2 font-barlow transition-all ${task.isCompleted ? "line-through text-neutral-500" : "text-neutral-300"}`}
+            >
+              {task.title}
             </p>
-          )}
+            {task.severity && (
+              <>
+                <span className="text-neutral-400">•</span>
+                <p
+                  className={`font-barlow ${returnSeverityStyle(task.severity as SeverityType)}`}
+                >
+                  {capitalizeStr(task.severity)}
+                </p>
+              </>
+            )}
+          </div>
+          <p className="font-barlow text-neutral-500">{task.description}</p>
         </div>
-        <p className="font-barlow text-neutral-500">{task.description}</p>
         {task.dueDate ? (
-          <p className="font-barlow font-extralight text-neutral-400 mt-1">
-            {dateFormatter(task.dueDate)}
+          <p
+            className={`font-barlow flex ${task.isExpired && "text-red-500/60"} items-center gap-2 text-xs font-extralight text-neutral-400 mt-1`}
+          >
+            <AlarmClock size={18} />
+            {dateFormatter(task.dueDate) == dateFormatter(new Date().toString())
+              ? "Today"
+              : dateFormatter(task.dueDate)}
           </p>
         ) : (
           <p className="font-barlow font-extralight items-center flex gap-2 text-xs text-neutral-400 mt-1">
