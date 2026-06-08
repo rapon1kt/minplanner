3ad35b897f1a6abd0abb1c2962430099ac635cd6
rapon1kt <shinnatensei@gmail.com>
2026-06-08 14:45:30 -0300

    feat: exporting record id utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as dateFormatter } from "./formatters/date-formater";
+export { getRecordId, getValueId } from "./record-id";
 export { default as sortTasks } from "./sorters/sort-tasks";
