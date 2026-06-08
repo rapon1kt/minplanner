7f1eefb9935f062dab1ce4aa9f7ad88bab364b3d
rapon1kt <shinnatensei@gmail.com>
2026-06-08 18:09:17 -0300

    feat: implementing new schemas of tag and exporting it

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,4 @@
 export { createTaskSchema } from "./taskSchema";
 export { deleteTaskSchema } from "./taskSchema";
 export { updateTaskSchema } from "./taskSchema";
-export { default as tagSchema } from "./tagSchema";
+export { createTagSchema, deleteTagSchema, updateTagSchema } from "./tagSchema";
