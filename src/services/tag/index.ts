385efd216b20c3f92df0a70750879a44b2b089e6
rapon1kt <shinnatensei@gmail.com>
2026-06-08 14:42:08 -0300

    feat: exporting tag services from tag folder

---
+++ src/services/tag/index.ts
@@ -0,0 +1,4 @@
+export { default as createTagService } from "./create-tag-service";
+export { default as deleteTagService } from "./delete-tag-service";
+export { default as getTagsService } from "./get-tags-service";
+export { default as updateTagService } from "./update-tag-service";
