d1d80c8992cad4cc2a59473bf8b5ec4f5a2c3479
rapon1kt <shinnatensei@gmail.com>
2026-06-26 23:18:24 -0300

    feat: adding components and container to index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Loading } from "./loading";
+export { default as TaskContainer } from "./task-container/task-container";
+export { default as TagContainer } from "./tag-container/tag-container";
+export { default as CalendarContainer } from "./calendar-container/calendar-container";
