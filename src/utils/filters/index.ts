6fea62e702e00a1d743d51a71ba7f0eeb9740363
rapon1kt <shinnatensei@gmail.com>
2026-06-29 14:41:18 -0300

    feat: exporting filters from folder

---
+++ src/utils/filters/index.ts
@@ -0,0 +1,15 @@
+export {
+  getDailyTasks,
+  isToday,
+  filterTasksByTag,
+  getWeekTasks,
+} from "./filter-tasks";
+export {
+  ALL_TAGS_FILTER,
+  COMPLETED_TAGS_FILTER,
+  OVERDUE_TAGS_FILTER,
+  THIS_WEEK_TAGS_FILTER,
+  TODAY_TAGS_FILTER,
+  UNTAGGED_TAGS_FILTER,
+  commonFilters,
+} from "./filter-tasks-constants";
